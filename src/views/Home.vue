9fc6be467e3806c13434f1ea90213eb7d043c110
apelissier <alexispelissier2002@gmail.com>
2022-01-19 12:43:59 +0100

    ajout base accueil

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="wrapper">
+      <div class="wrapper_box">
+        <div class="left_box">
+          <h2 style="color: white">Ha, te revoila !</h2>
+        </div>
+        <div class="right_box">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    //HelloWorld,
   }
 }
 </script>
+<style scoped>
+.wrapper{
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  min-height: 580px;
+  display: flex;
+  justify-content: center;
+  position: absolute;
+  align-items: center;
+}
+.wrapper_box{
+  min-height: 380px;
+  margin: 0 auto;
+  width: 784px;
+  height: 100px;
+  background-color: #36393f;
+  border: 2px solid;
+  border-radius: 5px;
+}
+body{
+  line-height: 1;
+}
+</style>
