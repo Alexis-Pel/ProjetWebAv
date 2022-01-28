1a8c57f48e0ccab149a6220ff7d1b1ed66465cd6
Alexis Rabiller <admin@MacBook-Pro-de-Alexis.local>
2022-01-28 13:13:14 +0100

    tristan le moche

--- projet-web-frontend/src/views/Settings.vue
+++ projet-web-frontend/src/views/Settings.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="main-wrapper">
-      
+      <div class="settings">
+        <div pseudo>
+          <h1>{{ inputPseudo }}</h1>
+          <input class="input" type="text" placeholder="nouveaux pseudo" v-model="inputPseudo">
+        </div>
+        <div>
+          <img :src="inputImg" >
+          <input class="input" type="text" placeholder="nouvelle image" v-model="inputImg">
+        </div>
+        <button v-on:click="submit(inputPseudo, inputImg)">sauvegarder</button>
+      </div>
   </div>
 </template>
 
@@ -14,27 +24,40 @@ export default {
   components: {},
 data() {
     return {
-      userLogged: {} //data of the logged user
+      userLogged: {}, //data of the logged user
+      inputPseudo: null,
+      inputImg: null,
     };
   },
   async beforeMount() {
-      var login = getCookie('token_login');
+      let login = getCookie('token_login');
       login = decrypt(login);
       try {
         await axios
           .get(`${server.baseURL}/users/user/${login}`)
-          .then((data) => (this.userLogged = data.data));
+          .then((data) => (this.userLogged = data.data, this.inputPseudo = data.data.pseudo, this.inputImg = data.data.img));
       } catch (e) {
           console.log(e)
       }
+  },
+  methods:{
+    async submit(inputPseudo, inputImg) {
+      try {
+        await axios.put(
+          `${server.baseURL}/users/update?customerID=${this.userLogged._id}`,
+          {
+            pseudo: inputPseudo,
+            img: inputImg,
+          }
+        );
+      } catch (error) {
+        console.log(error);
+      }
     },
+  }
 }
 </script>
 
-<style>
-.main-wrapper{
-  width: 100%;
-  height: 100%;
-  background-color: #36393e;
-}
+<style scoped>
+
 </style>
